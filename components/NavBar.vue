2a9cd0a7236b486928dc914811ddd84c57b19688
Natalia Kuchta <n.kuchta@onet.pl>
2024-08-23 22:43:24 +0400

    added background with navSearch and scaling on the page

---
+++ components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-grey p-4 shadow"
+  >
+    <a class="text-pink text-3xl font-mono" href="">CarFun</a>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+  <div>
+    <NavBar/>
+    <CarHero/>
+
+  </div>
+</template>
+
+<style scoped></style>
